c6299df06b641a1aa18fa87b0f96e9e4630187e3
otohusan <tamioguitar@gmail.com>
2025-08-05 16:42:43 +0900

    日付表記の変更

--- create_memos.sh
+++ create_memos.sh
@@ -7,15 +7,15 @@ MONTH=$2
 
 BASE_DIR=$(pwd)
 YEAR_DIR="$BASE_DIR/$YEAR"
-MONTH_DIR="$YEAR_DIR/${MONTH}月"
+MONTH_DIR="$YEAR_DIR/$(printf "%02d" $MONTH)月"
 
 mkdir -p "$MONTH_DIR"
 
 DAYS=$(cal $MONTH $YEAR | awk 'NF {DAYS = $NF}; END {print DAYS}')
 
 for DAY in $(seq 1 $DAYS); do
-  FILENAME="$MONTH_DIR/${DAY}日.md"
+  FILENAME="$MONTH_DIR/$(printf "%02d日.md" $DAY)"
   if [ ! -e "$FILENAME" ]; then
-    printf "# %04d年%d月%d日\n\n" "$YEAR" "$MONTH" "$DAY" >> "$FILENAME"
+    printf "# %04d年%02d月%02d日\n\n" "$YEAR" "$MONTH" "$DAY" >> "$FILENAME"
   fi
 done
